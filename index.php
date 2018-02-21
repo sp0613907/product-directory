9030488b4f766d546cd1bde0618f59feef87fa28
Steven Phillips <ninjamonkeys707@gmail.com>
2018-02-21 04:31:14 -0600

    changing code to match book

--- index.php
+++ index.php
@@ -7,14 +7,40 @@
         $category_id = 1;
     }
 
-    $category = getOne('SELECT * FROM categories WHERE id = :category_id',
-                        [':category_id', $category_id],
-                        $db);
-    $categories = getOne('SELECT * FROM categories ORDER BY id',
-                         [], $db);
-    $products = getOne('SELECT * FROM products WHERE category_id = :category_id ORDER BY id',
-                        [':category_id', $category_id],
-                        $db);
+    //category
+    $query = 'SELECT * FROM categories WHERE id = :category_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->execute();
+    $category = $statement->fetch();
+    $category_name = $category['name'];
+    $statement->closeCursor();
+
+    //categories
+    $query = 'SELECT * FROM categories ORDER BY id';
+    $statement = $db->prepare($query);
+    // $statement->bindValue(':category_id', $category_id);
+    $statement->execute();
+    $categories = $statement->fetchAll();
+    $statement->closeCursor();
+
+    //products
+    $query = 'SELECT * FROM products WHERE category_id = :category_id ORDER BY id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->execute();
+    $products = $statement->fetchAll();
+    $statement->closeCursor();
+
+
+    // $category = getOne('SELECT * FROM categories WHERE id = :category_id',
+    //                     [':category_id', $category_id],
+    //                     $db);
+    // $categories = getOne('SELECT * FROM categories ORDER BY id',
+    //                      [], $db);
+    // $products = getOne('SELECT * FROM products WHERE category_id = :category_id ORDER BY id',
+    //                     [':category_id', $category_id],
+    //                     $db);
 ?>  
 
 <!doctype html>
